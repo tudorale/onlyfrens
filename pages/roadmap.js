e4f73de7ecbbaaa97c729d159ae8e97a9ed39caf
Tudor Alexandru <tudorale2005@gmail.com>
2021-10-23 14:32:42 +0300

    roadmap page responsive

--- pages/roadmap.js
+++ pages/roadmap.js
@@ -13,7 +13,7 @@ function roadmap() {
                 <h1>General<br/>Roadmap</h1>
                 <div className={styles.roadmapGrid}>
                     <div className={styles.image}>
-                        <Image alt="roadmap illustration" src="/roadmap.png" width="572" height="1050"></Image>
+                        <Image quality="100" alt="roadmap illustration" src="/roadmap.png" width="572" height="1050"></Image>
                     </div>
                     <div className={styles.roadmapText}>
                         <svg width="34" height="1172" viewBox="0 0 34 1172" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -26,19 +26,19 @@ function roadmap() {
                         </svg>
 
                         <div className={styles.path}>
-                            <div>
+                            <div className={styles.pathOne}>
                                 <h1>Q4 2021</h1>
                                 <ul>
                                     <li>Social Media Campaign</li>
-                                    <li>Twitter, YouTube, Discord, Instagram, Reddit,<br/> Telegram, Facebook</li>
+                                    <li>Twitter, YouTube, Discord, Instagram, Reddit, Telegram, Facebook</li>
                                     <li>Rarity Index</li>
                                     <li>Mint Frens</li>
                                     <li>Secondary marketplace listings</li>
-                                    <li>MemeMarketplace, Solanart.io, Alpha.art, <br/> Solsea</li>
+                                    <li>MemeMarketplace, Solanart.io, Alpha.art, Solsea</li>
                                     <li>HowRare.Is Integration</li>
                                 </ul>
                             </div>
-                            <div style={{marginTop: "130px"}}>
+                            <div className={styles.pathTwo}>
                                 <h1>Q1 2022</h1>
                                 <ul>
                                     <li>Airdrops for Frens</li>
@@ -49,13 +49,13 @@ function roadmap() {
                                     <li>Continue development of OnlyFrens social network</li>
                                 </ul>
                             </div>
-                            <div style={{marginTop: "82px"}}>
+                            <div className={styles.pathThree}>
                                 <h1>Q2 2022</h1>
                                 <ul>
                                     <li>Launch OnlyFrens social network</li>
                                 </ul>
                             </div>
-                            <div style={{marginTop: "65px"}}>
+                            <div className={styles.pathFour}>
                                 <h1>Q3 2022</h1>
                                 <ul>
                                     <li>Partnerships and collaborations with other projects and influencers</li>
@@ -63,7 +63,7 @@ function roadmap() {
                                     <li>Continue OnlyFrens marketing campaign </li>
                                 </ul>
                             </div>
-                            <div style={{marginTop: "120px"}}>
+                            <div className={styles.pathFive}>
                                 <h1>Q4 2022 and Beyond</h1>
                                 <ul>
                                     <li>Eternal Gains</li>
